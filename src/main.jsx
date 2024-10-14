f1d70408ea8a2bf49c2eb0f7790c1916f746aa40
Veer Rajpoot <rahulksingh3907@gmail.com>
2024-10-14 12:14:36 +0530

    Refactor main.jsx to add OutOfBandwidth component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import OutOfBandwidth from "./components/OutOfBandwidth.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        {/* <App /> */}
+        <OutOfBandwidth />
+    </StrictMode>
+);
