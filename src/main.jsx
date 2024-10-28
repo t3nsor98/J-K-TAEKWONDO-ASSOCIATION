16e61918e47582b578f5a267957499ffc358c4b1
Veer Rajpoot <rahulksingh3907@gmail.com>
2024-10-28 19:50:11 +0530

    Refactor main.jsx to conditionally add OutOfBandwidth component

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import OutOfBandwidth from "./components/OutOfBandwidth.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        {/* <App /> */}
+        <App />
 
-        {window.location.host === "www.jkta.in" ? <OutOfBandwidth /> : <App />}
+        {/* {window.location.host === "www.jkta.in" ? <OutOfBandwidth /> : <App />} */}
     </StrictMode>
 );
