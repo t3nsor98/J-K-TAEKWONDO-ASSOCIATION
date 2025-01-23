4a3c20de9e1358e3f110107203a026097be3b2de
Veer Rajpoot <rahulksingh3907@gmail.com>
2025-01-23 23:34:07 +0530

    Comment out OutOfBandwidth component in main entry file

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import OutOfBandwidth from "./components/OutOfBandwidth.jsx";
+// import OutOfBandwidth from "./components/OutOfBandwidth.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <OutOfBandwidth />
-
+        {/* <OutOfBandwidth /> */}
+        <App />
         {/* {window.location.host === "www.jkta.in" ? <OutOfBandwidth /> : <App />} */}
     </StrictMode>
 );
